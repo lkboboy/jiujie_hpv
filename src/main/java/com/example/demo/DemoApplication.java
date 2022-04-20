b4de8a622ba2135f708dc6a479044ec233eb225f
lomok <lkboboy@foxmail.com>
2022-04-20 19:33:33 +0800

    修正哈提交的字段数据类型。。。

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,24 +3,28 @@ package com.example.demo;
 import com.alibaba.fastjson.JSONObject;
 import net.sf.json.JSONArray;
 import okhttp3.*;
+import org.apache.log4j.Logger;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.WebProperties;
+
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
 @SpringBootApplication
 public class DemoApplication {
+
+    private static final Logger log = Logger.getLogger(DemoApplication.class);
 
     //基本参数，自行Fiddler抓包分析获取，必要！
     private static String hos_code = "871958";     //医院id
     private static String dep_id = "379";          //医院的总疫苗选择页id
     private static String doc_id = "1334";         //医院的预约疫苗类型的id
-    private static String pat_id = "123345";       //就诊人信息id
-    private static String user_id = "12345";       //滇医通登录用户id
-    private static String Authorization = "DYT 11111111111.eyJ3ZWNoYXRfaWQiOjQ0MTU4MTQsInN1YnNjcmliZSI6MCwiZHpqX3N1YnNjcmliZSI6MCwib3BlbmlkIjoib19VMzZzeUxJQm12bF9pZm5HWkF3S0wya1ZFYyIsInRoaXJkX3VzZXJfaWQiOiIiLCJpc3MiOiJkeXQiLCJuZXdfc3Vic2NyaWJlIjoxLCJuZXdfb3BlbmlkIjoibzdMQ1g2QXN3SW9WdFNKd29qQ1pibDczLWd1VSIsImR6al9vcGVuaWQiOiIiLCJ1c2VyX2lkIjozNTI1MDYxLCJ3ZWNoYXRfb3Blbl9pZCI6Im9fVTM2c3lMSUJtdmxfaWZuR1pBd0tMMmtWRWMiLCJ1bmlvbl9pZCI6Im9OUXo0MFJBYlNPRjhQcUlndERFc3VSWkFmNzAiLCJtb2NrX29wZW5pZCI6ZmFsc2UsIm1pbmlfb3BlbmlkIjoib2lBNFA1SklNQzZYMjNPSUlrcHkweWJpdDN4QSIsImV4cCI6MTY0OTcyNjY0MiwiaWF0IjoxNjQ5NzIxMDQyfQ.nRl2jIba9wZju_IxNvfq4-968RWbO7qAIJ64Iyt-SAw";      //滇医通的登录认证
-    private static String x_uuid = "1111111111111111111";      //滇医通的登录认证
+    private static String pat_id = "111111";       //就诊人信息id
+    private static String user_id = "111111";       //滇医通登录用户id
+    private static String Authorization = "DYT eyJhkGciOiJIUzI1NiJ9.eyJ3ZWNoYXRfaWQiOjUxNjA0MjgsInN1YnNjcmliZSI6MCwiZHpqX3N1YnNjcmliZSI6MCwib3BlbmlkIjoibzdMQ1g2RzZUV2EzZU11RVZZQ0xqc0RRUHlBSSIsInRoaXJkX3VzZXJfaWQiOiIiLCJpc3MiOiJkeXQiLCJuZXdfc3Vic2NyaWJlIjoxLCJuZXdfb3BlbmlkIjoibzdMQ1g2RzZUV2EzZU11RVZZQ0xqc0RRUHlBSSIsInVzZXJfaWQiOjM2NjU3NjMsIndlY2hhdF9vcGVuX2lkIjoibzdMQ1g2RzZUV2EzZU11RVZZQ0xqc0RRUHlBSSIsInVuaW9uX2lkIjoib05RejQwYmtTYkxPZm5Ld2VHV1BINHdCb1ZjWSIsIm1vY2tfb3BlbmlkIjpmYWxzZSwibWluaV9vcGVuaWQiOiJvaUE0UDVKbWZpdDNyTDdFeWxVVHRLTUhxYWZNIiwiZXhwIjoxNjUwMjQ0Nzg4LCJpYXQiOjE2NTAyMzkxODh9.KzOfKL35LgMVMg7iAQ40KDGYTNsysniWuMjmpDUTNO0";      //滇医通的登录认证
+    private static String x_uuid = "6DC9C68F1A9AE61C33576429BE5BE53E";      //滇医通的登录认证
 
     private static boolean start = true;    //wile保险，怕重复提交表单
     private static Integer countI = 0;      //预约失败计次
@@ -59,19 +63,26 @@ public class DemoApplication {
             json = JSONObject.parseObject(tempResponse);
             jsonArray = JSONArray.fromObject(json.getJSONArray("data"));
             jsonArraySize = jsonArray.size();    //优化for循环的重复计算，减少遍历时间。
-            System.out.println("昆明市妇幼保健院(华山西路院区)-疫苗- 九价号探测");
-            System.out.println("现在系统时间：" + df.format(new Date()));
-            System.out.println("==============================================================================");
-            System.out.println(tempResponse);
+
+            log.info("昆明市妇幼保健院(华山西路院区)-疫苗- 九价号探测");
+
+            log.info("现在系统时间：" + df.format(new Date()));
+
+            log.info("==============================================================================");
+
+            log.info(tempResponse);
+
 
             //显示打印+疫苗号源列表遍历,从后往前遍历（抢），增加抢到概率。
             for (int i = 1; i <= jsonArraySize; i++) {
                 j = jsonArraySize - i; //优化for循环的重复计算，减少遍历时间。
-                System.out.println("日期：" + jsonArray.getJSONObject(j).getString("sch_date") +
+
+                log.info("日期：" + jsonArray.getJSONObject(j).getString("sch_date") +
                         "\t时间：" + jsonArray.getJSONObject(j).getString("cate_name") +
                         "\t时段：" + jsonArray.getJSONObject(j).getString("time_type").replace("1", "早上").replace("2", "下午") +
                         "\t总号源：" + jsonArray.getJSONObject(j).getString("src_max") +
                         "\t剩余号源：" + jsonArray.getJSONObject(j).getString("src_num")
+
                 );
 
                 //判断以上数组遍历是否存在剩余号源
@@ -83,7 +94,8 @@ public class DemoApplication {
 
                 }
             }
-            System.out.println("==============================================================================");
+
+            log.info("==============================================================================");
             //生成jar包，挂机，dos下刷屏显示用的！
 //                new ProcessBuilder("cmd", "/c", "cls")
 //                        .inheritIO()
@@ -94,22 +106,23 @@ public class DemoApplication {
 
     //抢九价疫苗，已拼接,换医院的话 改哈doc_name、hos_name 2个字段，其他不变，level_name字段页面 “疫苗接种预约|  ” 后面是否有 “疫苗” 两个字！
     public static boolean Get(String schedule_id, String sch_date, String time_type) throws IOException {
+        log.info(schedule_id + "==========" + sch_date + "==========" + time_type + "==========" + hos_code + "==========" + dep_id + "==========" + doc_id + "==========" + pat_id);
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         MediaType mediaType = MediaType.parse(" application/json");
         RequestBody body = RequestBody.create(mediaType, "{" +
                 "\"doc_name\": \"九价宫颈癌疫苗\"," +
                 "\"hos_name\": \"昆明市妇幼保健院(华山西路院区)-疫苗\"," +
-                "\"hos_code\": " + "\"" + hos_code + "\"," +                                 //医院id
+                "\"hos_code\": " + "\"" + hos_code + "\"," +                                    //医院id
                 "\"dep_name\": \"疫苗接种预约\"," +
                 "\"level_name\": \"疫苗\"," +
-                "\"dep_id\": " + "\"" + dep_id + "\"," +                                       //医院的总疫苗选择页id
-                "\"doc_id\": " + "\"" + doc_id + "\"," +                                       //医院的预约疫苗类型的id
-                "\"pat_id\": " + "\"" + pat_id + "\"," +                                       //就诊人信息id
-                "\"schedule_id\": " + "\"" + schedule_id + "\"," +                                   //排班id
+                "\"dep_id\": " + "\"" + dep_id + "\"," +                                        //医院的总疫苗选择页id
+                "\"doc_id\": " + "\"" + doc_id + "\"," +                                        //医院的预约疫苗类型的id
+                "\"pat_id\": " +  pat_id + "," +                                                //就诊人信息id
+                "\"schedule_id\": "  + schedule_id + "," +                                      //排班id
                 "\"jz_card\": \"\"," +
-                "\"sch_date\": " + "\"" + sch_date + "\"," +                              //排班时间
-                "\"time_type\": " + "\"" + time_type + "\"," +                                      //1为早上，2为下午
+                "\"sch_date\": " + "\"" + sch_date + "\"," +                                    //排班时间
+                "\"time_type\": " + "\"" + time_type + "\"," +                                  //1为早上，2为下午
                 "\"info\": \"\"," +
                 "\"ghf\": 0," +
                 "\"zlf\": 0," +
@@ -138,13 +151,16 @@ public class DemoApplication {
         Response response = client.newCall(request).execute();
         String tempResponse = response.body().string();
         JSONObject json = JSONObject.parseObject(tempResponse);
-        System.out.println(tempResponse);
+
+        log.info(tempResponse);
         if (json.getString("msg").equals("预约成功")) {
-            System.out.println("预约成功！");
+
+            log.info("预约成功！");
             start = false;
             return true;
         } else {
-            System.out.println("预约失败！");
+
+            log.info("预约失败！");
             countI++; //预约失败计次
             if (countI >= countJ) { //预约失败5次自动退出
                 start = false;
